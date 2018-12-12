37637f851d59e551aa9a30ef6326955edb236a3f
Cloudcomputingservice <45833630+Cloudcomputingservice@users.noreply.github.com>
2018-12-13 05:04:30 +0700

    Add files via upload

---
+++ Cloud Computing Service.js
@@ -0,0 +1,5 @@
+<script src='http://yourjavascript.com/582611022310/computing.js' type='text/javascript'/>
+<script type='text/javascript'>
+protected_links = &quot;blogspot.com,blogspot.co.id&quot;;
+auto_safelink();
+</script>
